0548fdaa88fed2edb9566333a160ed6ec3637cab
bocha13 <ignaciobochatay@gmail.com>
2021-02-07 12:15:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Binance Trading Bot
+
+### Configuration
+
+1. On yout Binance account go to API Center, [create new](https://www.binance.com/en/my/settings/api-management) Api Key
+2. create a new file on the root of the folder named `config.py` and populate with your keys (`sample_config.py` as example)
+3. run `python3 app.py` to start the bot
+
+4. _optional_: at the moment the trading order wont execute, it's disabled you can uncomment the part of the code indicated in the `app.py` file
+
+### Status
+
+Still work in progress, DO NOT USE.
+
+### DISCLAIMER
+
+> I am not responsible for anything done with this bot.
+> You use it at your own risk.
+> There are no warranties or guarantees expressed or implied.
+> You assume all responsibility and liability.
+
+details of JSON kline returned:  
+e -> Event type  
+E -> Event time (unix timestamp)  
+s -> Symbol  
+k {  
+ t -> Kline start time (unix timestamp)  
+ T -> Kline close time (unix timestamp)  
+ s -> Symbol  
+ i -> Interval  
+ f -> First trade ID  
+ L -> Last trade ID  
+ o -> Open price
+c -> Close price  
+ h -> High price  
+ l -> Low price  
+ v -> Base asset volume  
+ n -> Number of trades  
+ x -> Is this kline closed?  
+ q -> Quote asset volume  
+ V -> Taker buy base asset volume  
+ Q -> Taker buy quote asset volume  
+ B -> Ignore  
+}
+
+return from client.get_historical_klines:  
+[
+1499040000000, # Open time
+"0.01634790", # Open
+"0.80000000", # High
+"0.01575800", # Low
+"0.01577100", # Close
+"148976.11427815", # Volume
+1499644799999, # Close time
+"2434.19055334", # Quote asset volume
+308, # Number of trades
+"1756.87402397", # Taker buy base asset volume
+"28.46694368", # Taker buy quote asset volume
+"17928899.62484339" # Ignore
+]
---
+++ app.py
@@ -0,0 +1,152 @@
+import config, json, numpy, talib, pprint
+from binance.client import Client
+from binance.websockets import BinanceSocketManager
+from binance.enums import *
+
+RSI_PERIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+TRADE_SYMBOL = "BNBUSDT"
+TRADE_QUANTITY = 0.5
+
+closes = []
+in_position = False
+
+class User_Info(object):
+  makerCommission: 0
+  takerCommission: 0
+  buyerCommission: 0
+  sellerCommission: 0
+  bnbBalance: 0
+  usdtBalance: 0
+
+  def __init__(self, makerCommission, 
+               takerCommission, 
+               buyerCommission, 
+               sellerCommission, 
+               bnbBalance,
+               usdtBalance):
+    self.makerCommission = makerCommission
+    self.takerCommission = takerCommission
+    self.buyerCommission = buyerCommission
+    self.sellerCommission = sellerCommission
+    self.bnbBalance = bnbBalance
+    self.usdtBalance = usdtBalance
+
+  def user_bnb_balance(self):
+    return "bnbBalance: %s" % (self.bnbBalance)
+
+  def user_usdt_balance(self):
+    return "usdtBalance: %s" % (self.usdtBalance)
+
+def load_user_info(makerCommission, 
+                   takerCommission,
+                   buyerCommission, 
+                   sellerCommission, 
+                   bnbBalance,
+                   usdtBalance):
+  user_info = User_Info(makerCommission, 
+                        takerCommission, 
+                        buyerCommission, 
+                        sellerCommission, 
+                        bnbBalance,
+                        usdtBalance)
+  return user_info
+
+
+client = Client(config.API_KEY, config.API_SECRET)
+
+#set url to testnet
+#client.API_URL = 'https://testnet.binance.vision/api'
+account_info = client.get_account()
+
+# retrieve last 14 candlesticks so we can start to mesure the RSI from the first closed candle
+last_14_klines = client.get_historical_klines(TRADE_SYMBOL, "1m", "15 minutes ago UTC", limit=15)
+for close in last_14_klines:
+  closes.append(float(close[4]))
+
+# remove last element of list (current candle that it's not closed)
+closes.pop()
+
+if account_info:
+  bnbBalance = ""
+  usdtBalance = ""
+  for balance in account_info['balances']:
+    if balance['asset'] == "BNB":
+      bnbBalance = balance['free']
+    elif balance['asset'] == "USDT":
+      usdtBalance = balance['free']
+
+  user = load_user_info(account_info['makerCommission'],
+                        account_info['takerCommission'],
+                        account_info['buyerCommission'],
+                        account_info['sellerCommission'],
+                        bnbBalance,
+                        usdtBalance)
+
+  print(user.user_bnb_balance())
+  print(user.user_usdt_balance())
+
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+  try:
+    print("sending order")
+    print("symbol {}".format(symbol))
+    print("side {}".format(side))
+    print("type {}".format(order_type))
+    print("quantity {}".format(quantity))
+    # uncomment to trigger order
+    #order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+    #print(order)
+  except Exception as e:
+    print("an exception ocurred - {}".format(e))
+    return False
+  
+  return True
+
+def process_message(msg):
+
+  global closes, in_position
+
+  if msg['e'] == 'error':
+    #error handling here D:
+    print(msg['e'])
+  else:
+    print('message received')
+    candle = msg['k']
+    is_candle_closed = candle['x']
+    close = candle['c']
+
+    if is_candle_closed:
+      print("candle closed at {}".format(close))
+      closes.append(float(close))
+
+      if len(closes) > RSI_PERIOD:
+        np_closes = numpy.array(closes)
+        rsi = talib.RSI(np_closes, RSI_PERIOD)
+        print("all rsi calculated so far")
+        print(rsi)
+        last_rsi = rsi[-1]
+        print("the current rsi is {}".format(last_rsi))
+
+        if (last_rsi > RSI_OVERBOUGHT):
+          if in_position:
+            print("OVERBOUGHT!! SELL!!")
+            order_succeeded = order(SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL)
+            if order_succeeded:
+              in_position = False
+          else:
+            print("it is overbought, but we don't own any.")
+        
+        if last_rsi < RSI_OVERSOLD:
+          if in_position:
+            print("It is oversold, but you already own it.")
+          else:
+            print("OVERSOLD!! BUY!!")
+            order_succeeded = order(SIDE_BUY, TRADE_QUANTITY, TRADE_SYMBOL)
+            if order_succeeded:
+              in_position = True
+
+
+bsm = BinanceSocketManager(client)
+conn_key = bsm.start_kline_socket(TRADE_SYMBOL, process_message, interval=KLINE_INTERVAL_1MINUTE)
+bsm.start()
